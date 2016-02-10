d598c81b9b2cf98aa840cf8ec176092cc5a8b8ce
gabriel <ggonzalv@lion.lmu.edu>
2016-02-09 22:42:55 -0800

    add skeleton html/css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
 $(function(){
-  var $testCard = $("<img>").addClass("center-block");
 
-  $.get("https://api.deckbrew.com/mtg/cards/reflector-mage",function(cards){
-    $testCard.attr("src",cards.editions[0].image_url);
-    $(".container").append($testCard);
-  });
 });
