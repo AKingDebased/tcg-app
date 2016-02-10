c7a8e412eebd3c304039dbc29eb2da38c4fe0dde
gabriel <ggonzalv@lion.lmu.edu>
2016-02-10 00:07:46 -0800

    naively begin deck builder
    drafted up a simple card text input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(function(){
+  var splitCardSelection = function(cardSelections){
+    //extract card amount and name from string
+    return cardSelections.map(function(cardSelection){
+      var splitSelection = cardSelection.split(" ");
+      var amount = parseInt(splitSelection.splice(0,1));
 
+      return {"amount": amount,"name": splitSelection.join(" ")}
+    });
+  }
+
+  $(".add-to-deck").click(function(){
+    var cardSelections = $(".deck-builder textarea").val().split("\n");
+    console.log(splitCardSelection(cardSelections));
+  });
 });
