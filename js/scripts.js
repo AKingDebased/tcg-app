b526593e1e90d355fdd4d56249de8daf1d8b82ba
gabriel <ggonzalv@lion.lmu.edu>
2016-02-10 13:03:22 -0800

    deck builder to card pool

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,13 @@ $(function(){
     return cardSelections.map(function(cardSelection){
       var splitSelection = cardSelection.split(" ");
       var amount = isNaN(parseInt(cardSelections[0])) === true ? NaN : parseInt(splitSelection.splice(0,1));
-    
+
       return {"amount": amount,"name": splitSelection.join(" ")}
     });
-
   }
 
-  $(".add-to-deck").click(function(){
-    console.log(splitCardSelection($(".deck-builder textarea").val()));
-    $(".deck-builder textarea").val("");
+  $(".add-to-pool").click(function(){
+    console.log(splitCardSelection($(".pool-builder textarea").val()));
+    $(".pool-builder textarea").val("");
   });
 });
