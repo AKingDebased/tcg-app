4989d7864a549a139f9e7e6578b2941860e39976
gabriel <ggonzalv@lion.lmu.edu>
2016-02-09 23:53:23 -0800

    add default amount to deck builder
    if no number is given, the array is not spliced

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,18 @@ $(function(){
     //extract card amount and name from string
     var cardSelections = selectionsStr.split("\n");
 
+    console.log(cardSelections);
     return cardSelections.map(function(cardSelection){
       var splitSelection = cardSelection.split(" ");
-      var amount = parseInt(splitSelection.splice(0,1));
-
+      var amount = isNaN(parseInt(cardSelections[0])) === true ? NaN : parseInt(splitSelection.splice(0,1));
+    
       return {"amount": amount,"name": splitSelection.join(" ")}
     });
+
   }
 
   $(".add-to-deck").click(function(){
-    splitCardSelection($(".deck-builder textarea").val());
+    console.log(splitCardSelection($(".deck-builder textarea").val()));
     $(".deck-builder textarea").val("");
   });
 });
